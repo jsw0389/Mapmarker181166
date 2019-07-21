8d2c98b6164364b1dac6b877421c4f331bdb4ed3
jsw0389 <46648772+jsw0389@users.noreply.github.com>
2019-07-21 23:53:48 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -365,7 +365,7 @@ function success(pos) {
   // 지도 중심을 부드럽게 이동시킵니다
   // 만약 이동할 거리가 지도 화면보다 크면 부드러운 효과 없이 이동합니다
   map.panTo(moveLatLon);
-}
+} 
 
 function error(err) {
   console.warn('ERROR(' + err.code + '): ' + err.message);
